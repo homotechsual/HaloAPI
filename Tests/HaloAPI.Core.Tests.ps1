d36f0e6cdd1ca0a507e7a11ac24e6843c9379ee5
Mikey O'Toole <mikey@mjco.uk>
2021-08-11 20:44:25 +0100

    Fix tests

--- Tests/HaloAPI.Core.Tests.ps1
+++ Tests/HaloAPI.Core.Tests.ps1
@@ -6,12 +6,13 @@
 BeforeAll {
     $ModulePath = Split-Path -Parent -Path (Split-Path -Parent -Path $PSCommandPath)
     $ModuleName = 'HaloAPI'
-    $ManifestPath = "$ModulePath\$ModuleName.psd1"
+    $ManifestPath = "$($ModulePath)\$($ModuleName).psd1"
     if (Get-Module -Name $ModuleName) {
         Remove-Module $ModuleName -Force
     }
     Import-Module $ManifestPath -Verbose:$False
-    $Script:ManifestHash = Get-Content $ManifestPath -Raw
+    $Script:ModuleInformation = Import-module -Name $ManifestPath -PassThru
+
 }
 
 # Test that the manifest is generally correct. Not a functional test.
@@ -23,35 +24,39 @@ Describe 'Core' {
     }
 
     It 'Root module is correct' {
-        $Script:ManifestHash.RootModule | Should -Be ".\$($ModuleName).psm1"
+        $Script:ModuleInformation.RootModule | Should -Be ".\$($ModuleName).psm1"
     }
 
     It 'Has a description' {
-        $Script:ManifestHash.Description | Should -Not -BeNullOrEmpty
+        $Script:ModuleInformation.Description | Should -Not -BeNullOrEmpty
     }
 
     It 'GUID is correct' {
-        $Script:ManifestHash.GUID | Should -Be '8bc83215-4735-4029-9f40-e05fe3e8f73b'
+        $Script:ModuleInformation.GUID | Should -Be '8bc83215-4735-4029-9f40-e05fe3e8f73b'
     }
 
     It 'Version is valid' {
-        $Script:ManifestHash.ModuleVersion -As [Version] | Should -Not -BeNullOrEmpty
+        $Script:ModuleInformation.Version -As [Version] | Should -Not -BeNullOrEmpty
     }
 
     It 'Copyright is present' {
-        $Script:ManifestHash.Copyright | Should -Not -BeNullOrEmpty
+        $Script:ModuleInformation.Copyright | Should -Not -BeNullOrEmpty
     }
 
     It 'License URI is correct' {
-        $Script:ManifestHash.PrivateData.Values.LicenseUri | Should -Be 'https://haloapi.mit-license.org/'
+        $Script:ModuleInformation.LicenseUri | Should -Be 'https://haloapi.mit-license.org/'
     }
 
     It 'Project URI is correct' {
-        $Script:ManifestHash.PrivateData.Values.ProjectUri | Should -Be 'https://github.com/homotechsual/HaloAPI'
+        $Script:ModuleInformation.ProjectUri | Should -Be 'https://github.com/homotechsual/HaloAPI'
+    }
+
+    It 'PowerShell Gallery tags is not empty' {
+        $Script:ModuleInformation.Tags.count | Should -Not -BeNullOrEmpty 
     }
 
     It 'PowerShell Gallery tags do not contain spaces' {
-        foreach ($Tag in $Script:ManifestHash.PrivateData.Values.tags) {
+        foreach ($Tag in $Script:ModuleInformation.Tags) {
             $Tag -NotMatch '\s' | Should -Be $True
         }
     }
